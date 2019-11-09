bd2d5a039416b99ee217d84017c8f588dc2237d2
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-09 16:01:45 +0200

    2.1  Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. А программа сранивает два введенных числа и выводит на консоль результат сравнения (два числа равны, первое число больше второго или первое число меньше второго).
    Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. А программа сранивает два введенных числа и выводит на консоль результат сравнения (два числа равны, первое число больше второго или первое число меньше второго).

---
+++ 2.1 Couditional Constructions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2._1_Couditional_Constructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1-oe число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2-oe число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 > num2)
+            {
+                Console.WriteLine("1-oe Число больше 2-го");
+            }
+            else if(num1 < num2)
+            {
+                Console.WriteLine("2-oe число больше 1 - го");
+            }
+            else
+            {
+                Console.WriteLine("Числа равны");
+            }
+            Console.ReadKey();
+        }
+    }
+}
