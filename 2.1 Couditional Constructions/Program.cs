e731e6907f20e93957426880b049f7bafc8b3d98
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-09 16:22:32 +0200

    2.3 Task Couditional Constructions
    Напишите консольную программу, в которую пользователь вводит с клавиатуры число. Если число либо равно 5, либо равно 10, то программа выводит "Число либо равно 5, либо равно 10". Иначе программа выводит сообщение "Неизвестное число".

--- 2.1 Couditional Constructions/Program.cs
+++ 2.1 Couditional Constructions/Program.cs
@@ -6,12 +6,12 @@ namespace _2._1_Couditional_Constructions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите 1-oe число");
+            Console.WriteLine("Введите ");
             int num1 = Convert.ToInt32(Console.ReadLine());
-           
-            if(num1 > 5 && 10 > num1)
+
+            if (num1 == 5 || 10 == num1)
             {
-                Console.WriteLine("Число больше 5 и меньше 10");
+                Console.WriteLine("Число либо равно 5, либо равно 10");
             }
             else
             {
