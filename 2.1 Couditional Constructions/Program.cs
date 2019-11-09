92610a844df23192ad68131b6a7a31829ca9fb90
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-09 16:33:23 +0200

    2.5  Conditional Constructions
    Изменим предыдущую задачу. Допустим, банк периодически начисляет по всем вкладам кроме процентов бонусы. И, допустим, сейчас банк решил доначислить по всем вкладам 15 единиц вне зависимости от их суммы. Измените программу таким образом, чтобы к финальной суме дочислялись бонусы.

--- 2.1 Couditional Constructions/Program.cs
+++ 2.1 Couditional Constructions/Program.cs
@@ -8,19 +8,21 @@ namespace _2._1_Couditional_Constructions
         {
             Console.WriteLine("Введите cумму вклада");
             double sum = Convert.ToDouble(Console.ReadLine());
-
+            double procent = 0;
             if (sum < 100)
             {
-                sum += sum * 0.05;
+                procent = 0.05;
             }
             else if(sum >= 100 && sum <= 200)
             {
-                sum += sum * 0.07;
+                procent = 0.07;
             }
             else
             {
-                sum += sum * 0.1;
+                procent = 0.1;
             }
+            sum += sum * procent + 15;
+
             Console.WriteLine($"Сумму вклада после начисления процентов: {sum}");
             Console.ReadKey();
         }
