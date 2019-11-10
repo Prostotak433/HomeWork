db771ebbc46d5354c09daccae1c4232ad5058c2a
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-10 15:41:15 +0200

    2. 1 TaskCycle
    За каждый месяц банк начисляет к сумме вклада 7% от суммы. Напишите консольную программу, в которую пользователь вводит сумму вклада и количество месяцев. А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
    
    Для вычисления суммы с учетом процентов используйте цикл for. Для ввода суммы вклада используйте выражение Convert.ToDecimal(Console.ReadLine()) (сумма вклада будет представлять тип decimal).

---
+++ Task_1_Cycle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1_Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите количество месяцев");
+            int period = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i<=period; i++)
+            {
+                sum += sum * 0.07M;
+            }
+            Console.WriteLine($"После {period} сумма вклада состовялет {sum}");
+            Console.ReadKey();
+        }
+    }
+}
