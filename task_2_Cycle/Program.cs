5990ab0026ecd5f347b0b5d3a8f8c147838e0400
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-10 15:51:59 +0200

    2.2 Task Cycke
    Перепишите предыдущую программу, только вместо цикла for используйте цикл while.

---
+++ task_2_Cycle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_2_Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада: ");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите срок вклада в месяцах: ");
+            int period = Convert.ToInt32(Console.ReadLine());
+            int i = 1;                    //  i = period
+            while(i < period)             // (i > 0)
+            {
+                sum += sum * 0.07M;
+                i++;
+            }
+            Console.WriteLine($"После {period} месяцев сумма вклада составит {sum}");
+            Console.ReadKey();
+        }
+    }
+}
