ebcb161ea7ec4d25cb9609f0e577f02bbe854652
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-10 16:10:11 +0200

    2.3 Task Cycle
    Напишите программу, которая выводит на консоль таблицу умножения

---
+++ Task_3_Cycle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_3_Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++) 
+                {
+                    Console.Write($"{i * j}\t");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
