4d0f5db5e0b9859c4503f6a05acdfb4daf6a7ef5
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-10 16:21:53 +0200

    2.4 Task Cycle
    Напишите программу, в которую пользователь вводит два числа и выводит результат их умножения. При этом программа должны запрашивать у пользователя ввод чисел, пока оба вводимых числа не окажутся в диапазоне от 0 до 10. Если введенные числа окажутся больше 10 или меньше 0, то программа должна вывести пользователю о том, что введенные числа недопустимы, и повторно запросить у пользователя ввод двух чисел. Если введенные числа принадлежат диапазону от 0 до 10, то программа выводит результат умножения.
    
    Для организации ввода чисел используйте бесконечный цикл while и оператор break.

---
+++ Task_4_Cycle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_4_Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите первое число:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10)
+                {
+                    Console.WriteLine("Числа не в диапазоне");
+                }
+                else
+                {
+                    Console.WriteLine($"{num1}*{num2} = {num1 * num2}");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
