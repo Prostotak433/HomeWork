d86857a75f3f642dca773d29888f1e2c7f8a5938
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-18 22:05:41 +0200

    3.2 Task Operator Overload

---
+++ 2 Task  Operator Overload/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2_Task__Operator_Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bread bread = new Bread { Weight = 80 };
+            Butter butter = new Butter { Weight = 20 };
+            Sandwich sandwich = bread + butter;
+            Console.WriteLine(sandwich.Weight);
+        }
+    }
+    class Bread
+    {
+        public int Weight { get; set; } 
+        public static Sandwich operator +(Bread bread, Butter butter)
+        {
+            return new Sandwich { Weight = bread.Weight + butter.Weight };
+        }
+    }
+
+    
+    class Butter
+    {
+        public int Weight { get; set; }
+    }
+
+    // бутерброт
+    class Sandwich
+    {
+        public int Weight { get; set; } 
+    }
+}
