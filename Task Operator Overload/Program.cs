665cf292ce6a4a8870a6bc4dd3816873dba9e1ed
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-18 18:51:10 +0200

    3.1. Task Operator Overload

---
+++ Task Operator Overload/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_Operator_Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            State Russian = new State { Area = 300, Population = 3000 };
+            State Ukranian = new State { Area = 100, Population = 2000 };
+            State SSSR = Ukranian + Russian;
+            Console.WriteLine(SSSR.Population);
+            if (Russian > Ukranian)
+            {
+                Console.WriteLine("Russia bol6we Ukr");
+            }
+            else
+            {
+                Console.WriteLine("Russia men6we Ukr");
+            }
+        }
+    }
+}
---
+++ Task Operator Overload/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Operator_Overload
+{
+    class State
+    {
+        public decimal Population { get; set; }
+        public decimal Area { get; set; }
+        public static State operator +(State s1, State s2)
+        {
+            return new State
+            {
+                Population = s1.Population + s2.Population,
+                Area = s1.Area + s2.Area
+            };
+        }
+        public static bool operator >(State s1, State s2)
+        {
+            return s1.Area > s2.Area;
+        }
+        public static bool operator <(State s1, State s2)
+        {
+            return s1.Area < s2.Area;
+        }
+    }
+}
