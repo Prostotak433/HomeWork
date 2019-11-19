ad67ddb7caf53fd3d67b8d3e257a5627ce091e6c
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-19 16:07:01 +0200

    3.1. Task Index

---
+++ 3.1  Task Index/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3._1_Task_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team dnepr = new Team();
+            dnepr[0] = new Player { Name = "Tom", Number = 13 };
+        }
+    }
+    class Player
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+    class Team
+    {
+        Player[] players = new Player[11];
+        public Player this[int index]
+        {
+            get { return players[index]; }
+            set { players[index] = value; }
+        }
+    }
+}
