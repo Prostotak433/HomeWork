7b724562c65e1295f7826e47301a18c0e44e5066
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-21 15:35:30 +0200

    3.1  Task Overloading Type Conversion Operations

---
+++ Overloading Type Conversion Operations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Overloading_Type_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            int x = 34;
+            clock.Hours = x % 24;
+            x = clock.Hours;
+        }
+    }
+    class Clock
+    {
+        public int Hours { get; set; }
+        public static implicit operator Clock(int x)
+        {
+            return new Clock { Hours = x % 24 };
+        }
+        public static explicit operator int(Clock clock)
+        {
+            return clock.Hours;
+        }
+            
+    }
+}
