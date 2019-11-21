8a0be490a2d248a82bf7a94a8f729148a23f1cc7
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-21 15:47:40 +0200

    3.2 Task Overloading Type Conversion Operations

---
+++ 2Task Overloading Type Conversion Operations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2Task_Overloading_Type_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celcius celcius = new Celcius { Gradus = 25 };
+            Fahrenheit fahrenheit = celcius;
+            Console.WriteLine(fahrenheit.Gradus);
+            celcius = fahrenheit;
+            Console.WriteLine(celcius.Gradus);
+        }
+    }
+    class Celcius
+    {
+        public double Gradus { get; set; }
+        public static implicit operator Celcius(Fahrenheit fahrenheit)
+        {
+            return new Celcius { Gradus = 5.0 / 9.0 * (fahrenheit.Gradus - 32) };
+        }
+        public static implicit operator Fahrenheit(Celcius celcius)
+        {
+            return new Fahrenheit { Gradus = 9.0 / 5 * (celcius.Gradus + 32) };
+        }
+        
+    }
+    class Fahrenheit
+    {
+        public double Gradus { get; set; }
+    }
+}
