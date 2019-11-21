2eaab365aafdcab04029e8d16d84d4dcb2061bad
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-21 15:59:08 +0200

    3.3 Task Overloading Type Conversion Operations

---
+++ 3 Task Overloading Type Conversion Operations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _3_Task_Overloading_Type_Conversion_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dollar dollar1 = new Dollar { Sum = 100 };
+            Euro euro = dollar1;
+            Console.WriteLine(euro.Sum);
+            
+
+        }
+    }
+    class Dollar
+    {
+        public decimal Sum { get; set; }
+
+    }
+    class Euro
+    {
+        public decimal Sum { get; set; }
+        public static implicit operator Euro(Dollar dollar)
+        {
+            return new Euro { Sum = dollar.Sum / 1.14M };
+        }
+        public static explicit operator Dollar(Euro euro)
+        {
+            return new Dollar { Sum = euro.Sum * 1.14M };
+        }
+    }
+}
