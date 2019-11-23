5fd422d3e7581c3e2f2184157a7a698fdc846185
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-23 16:49:01 +0200

    3.1 Task Generics

---
+++ 3.1 Task Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3._1_Task_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Instantiator<T>
+    {
+        public T instance;
+        public Instantiator()
+        {
+            instance = default (T);
+        }
+    }
+}
