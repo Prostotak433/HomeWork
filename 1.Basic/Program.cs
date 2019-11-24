5fe324196bf015abdc19e1774e9314905131ca48
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-24 14:38:25 +0200

    1.2.3.4.5 Basic

---
+++ 1.Basic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1.Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Alexandra Abramovna");  // 1
+
+            Console.WriteLine(1+1);   // 2
+
+            Console.WriteLine(5/3);  // 3
+
+            Console.WriteLine(-1+4*6);
+            Console.WriteLine((35 + 5) % 7);
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2); //4
+
+            int num1, num2, num3;
+            Console.WriteLine("Введите 1-ое число");
+            num1 = (Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите 2-ое число");
+            num2 = (Convert.ToInt32(Console.ReadLine()));
+            num3 = num1;
+            num1 = num2;
+            num2 = num3;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : " + num1);
+            Console.Write("\nSecond Number : " + num2);
+            Console.Read();
+        }
+    }
+}
