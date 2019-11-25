4aeae4a26e45f857735afaeeef92149555eb9801
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-25 16:37:51 +0200

    14. 15 Basic

---
+++ 5. Basic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5._Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(del("w3resource", 1));
+            Console.WriteLine(del("w3resource", 9));
+            Console.WriteLine(del("w3resource", 0));
+
+            Console.WriteLine("Градусов по цельсию");
+            int cel = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Кельвину = {0}", cel + 273);
+            Console.WriteLine("Фаренгейту = {0}", cel * 18 / 10 + 32);
+
+        }
+        public static string del(string str, int n)
+        {
+            return str.Remove(n, 1);
+        }
+        
+    }
+}
