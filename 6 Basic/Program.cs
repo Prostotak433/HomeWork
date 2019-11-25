76447cc0d2fc0cbda2a722b636904bd4bfa09507
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-25 16:57:32 +0200

    16. Basic  REMOVE

---
+++ 6 Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        public static string first_last(string ustr)
+        {
+            
+        }
+    }
+}
