9bfb151863e813bfaa8023e08c0d701ed2833f29
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-25 16:11:22 +0200

    11.12.13 Basic

---
+++ 4. Basic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4._Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш возраст");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вроде вы выглядите старше {0}", x);  // 11
+
+            Console.WriteLine("Введите число");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0}{0}", y);
+            Console.WriteLine("{0} {0} {0} {0}", y);
+            Console.WriteLine("{0}{0}{0}{0}", y);
+            Console.WriteLine("{0} {0} {0} {0}", y);       //12
+
+            Console.WriteLine("Введите число");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0}", z);
+            Console.WriteLine("{0}   {0}", z);
+            Console.WriteLine("{0}   {0}", z);
+            Console.WriteLine("{0}   {0}", z);
+            Console.WriteLine("{0}{0}{0}", z);      //13
+        }
+    }
+}
