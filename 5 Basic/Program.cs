b095a355c5629d583185453fade9a90f37a3fb09
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-27 21:07:59 +0200

    17. Basic

---
+++ 5 Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int l = 0;
+            Console.Write("Введите строу");
+            str = Console.ReadLine();
+            if(str.Length >0)
+            {
+                var a = str.Substring(0, 1);
+                Console.WriteLine(a + str + a);
+            }
+        }
+    }
+}
