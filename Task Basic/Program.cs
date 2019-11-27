7dcaa9132c084c3c51219b83610b2754579aa11e
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-27 21:20:13 +0200

    18. Basic

---
+++ Task Basic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите первое число");
+            int a  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+}
