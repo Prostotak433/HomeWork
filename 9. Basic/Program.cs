90060a322a5bd198a33ac416bff36bea64cdda8d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-27 22:46:24 +0200

    23. Basic

---
+++ 9. Basic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _9._Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string str = Console.ReadLine();
+            Console.WriteLine(str.ToLower());
+        }
+    }
+}
