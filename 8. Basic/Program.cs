6b2f3e5f5389cc7d0ee3dc44485f8da63e67a5b9
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-27 22:37:47 +0200

    22. Basic

---
+++ 8. Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8._Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nВведите целое число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(res(x));
+        }
+        public static bool res(int x)
+        {
+            if (Math.Abs(x - 100) <= 20 || Math.Abs(x - 200) <= 20)
+                return true;
+            return false;
+        }
+    }
+}
