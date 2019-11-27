4496cf8c5bd1a03122f9de10c83743381dca43cb
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-27 21:45:03 +0200

    19. Basic

---
+++ Basic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 -ое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 -ое число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result = Sum(a, b);
+            Console.WriteLine(result);
+        }
+        public static int Sum(int a, int b)
+        {
+            return a == b ? (a + b) * 3 : (a + b);
+        }
+    }
+    
+}
