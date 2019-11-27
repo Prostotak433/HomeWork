e1c7aea8eab339e4d472d8a557f2a290a3c65226
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-27 23:07:31 +0200

    24.Basic

---
+++ 10 Basic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string str = Console.ReadLine();
+            string[] mass = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string zam = "";
+            int b = 0;
+
+            foreach(string a in mass)
+            {
+                if(a.Length>0)
+                {
+                    zam = a;
+                    b = a.Length;  
+                }
+            }
+            Console.WriteLine(zam);
+        }
+    }
+}
