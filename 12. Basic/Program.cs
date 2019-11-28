0d16068a95cdcfca43ecdc9221c87a8479c55532
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-28 21:55:44 +0200

    35. Basic

---
+++ 12. Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _12._Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1-ое число для проверки меньше 100");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2-ое число для проверки меньше 200");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((a < 100 && b < 200));
+        }
+    }
+}
