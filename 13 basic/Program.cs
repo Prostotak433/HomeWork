35b4f43316c545f8151758a6482a7a6faa934419
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2019-11-28 22:00:20 +0200

    36. Basic

---
+++ 13 basic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _13_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((-10 < a && a < 10));
+        }
+    }
+}
