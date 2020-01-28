58b5e9267aa8f5aac5252cbe2241834b3093aa84
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-28 20:06:03 +0200

    2. Task (For Loop)

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,11 +6,13 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-
-                for (int i = 1; i <= 10; i++)
-                {
-                    Console.Write("{0} ", i);
-                }
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                Console.Write("{0} ", i);
+            }
+            Console.Write("Cумма: {0}", sum);
                 
            
         }
