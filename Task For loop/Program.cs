bd54f1467a6e278d6b8d24833b728932e8562530
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-28 19:56:52 +0200

    1. Task (For Loop)

---
+++ Task For loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.Write("{0} ", i);
+                }
+                
+           
+        }
+    }
+    
+}
