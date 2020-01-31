bb55046c0d656b9fc942935a20d8c87b54f8d4f2
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-01-31 17:23:56 +0200

    16. Task ( For Loop)

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,16 +6,17 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            int f = 1; 
+            int sum = 0; 
             Console.Write("Введите число длины: ");
             int i = Convert.ToInt32(Console.ReadLine());
             for (int n = 1; n <= i; n++)
             {
-                f = f * n;
+                Console.Write("{0} ", 2 * n);
+                sum += 2*n;
 
-            }  
-            
-            Console.WriteLine("Факториал числа {0} равен {1} ", i, f);   
+            }
+
+            Console.WriteLine("Сумма натуральных чисел {0} равно {1}",i, sum);   
             
             Console.Read();
           
