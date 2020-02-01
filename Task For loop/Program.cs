1ebcda6d9c75dd69c73a07dd745c96a7aa79a58f
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-01 14:35:30 +0200

    20. Task ( For Loop)

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,17 +6,17 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            
-          
-            Console.Write("Введите число терминов ");
-            double a = Convert.ToInt32(Console.ReadLine());
-            double sum = 0;
-            for(double n =1; n<=a; n++)
+            Console.Write("Длина пирамиды ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < a; i++)
             {
-                Console.Write("1/{0}+",n);
-                sum = sum + 1 / n;
+                for(int j=1; j<=a-i; j++ )
+                Console.Write(" ");
+                    for(int j=1; j<=2*i-1; j++)
+                     Console.Write("*");
+                Console.Write("\n");
             }
-            Console.WriteLine("\nКоличество терминов {0}\nОжидаеммый результат = {1} ", a,sum);
+            
            
             
             Console.Read();
