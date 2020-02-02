1cf1d5cbaa9718a0bd2db3ef80cd97ddb0724797
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-02 17:20:43 +0200

    24. Task ( For Loop )

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,19 +6,20 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            double sum = 1, peremen = 1;
-            Console.Write("Введите значение Х: ");
-            double x = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите количество терминов: ");
-            double a = Convert.ToInt32(Console.ReadLine());
-            for(double i = 1; i<a;i++)
+            Console.Write("Значение Х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Количество Терминов: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double t = 1, sum=0;
+            for(double i = 1, step =1; i < a+1; i++)
             {
-                peremen = peremen * x / i;
-                sum = sum + peremen;
+                double math = Math.Pow(x, step);
+                sum = sum + math * t;
+                t = t * (-1);
+                step = step * 2;
             }
-            Console.Write("\nCумма {0}\nКоличество терминов {1}\nЗначение Х: {2} ", sum, a, x);
-            Console.Read();
+            Console.Write("\nСумма {0}\nКоличество терминов {1}\nX={2} ", sum, a, x);
         }
     }
-    
+
 }
