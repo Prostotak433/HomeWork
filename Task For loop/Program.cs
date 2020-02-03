9c0c588dd3fc27c5188f704236245e18e3790b4c
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-03 17:02:22 +0200

    30. Task ( For Loop )

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,19 +6,29 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            int num, r, sum = 0, temp;
-                        Console.Write("Input  a number: ");
-            num = Convert.ToInt32(Console.ReadLine());
+            int num, r, sum, temp;
+            int stno, enno;
+            Console.Write("Input starting number of range: ");
+            stno = Convert.ToInt32(Console.ReadLine());
 
-            for (temp = num; num != 0; num = num / 10)
+            Console.Write("Input ending number of range : ");
+            enno = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Armstrong numbers in given range are: ");
+            for (num = stno; num <= enno; num++)
             {
-                r = num % 10;
-                sum = sum + (r * r * r);
+                temp = num;
+                sum = 0;
+
+                while (temp != 0)
+                {
+                    r = temp % 10;
+                    temp = temp / 10;
+                    sum = sum + (r * r * r);
+                }
+                if (sum == num)
+                    Console.Write("{0} ", num);
             }
-            if (sum == temp)
-                Console.Write("{0} is an Armstrong number.\n", temp);
-            else
-                Console.Write("{0} is not an Armstrong number.\n", temp);
         }
     }
 }
