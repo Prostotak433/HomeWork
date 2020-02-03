5532821e3717039d1c450540dd96b225ed156a94
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-03 16:02:48 +0200

    27. Task ( For Loop )

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,16 +6,18 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            int sum = 0, j=1;
-            Console.Write("Введите количество терминов : ");
+            int sum = 0;
+            Console.Write("Введите число : ");
             int a = Convert.ToInt32(Console.ReadLine());
-            for(int i =1; i<=a;i++)
+            for (int i = 1; i <= a; i++)
             {
-                sum += j;
-                Console.Write("{0} ", j);
-                j = j * 10 + 1;
+                if (a % i == 0)
+                {
+                    sum += i;
+                    Console.Write("{0} ", i);
+                }
             }
-            Console.Write("Натуральное число квадрата до {0} терминов = {1} ", j, sum);
+            Console.Write("Сумма делителей {0} ", sum);
         }
     }
 }
