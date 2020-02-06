589e3e2086ea74be031e060db9ea7cd2da34ce48
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-06 18:09:36 +0200

    40. Task ( For Loop )

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,22 +6,26 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            int sum = 0, i;
-            Console.Write("Введите число : ");
-            int amin = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите число : ");
-            int amax = Convert.ToInt32(Console.ReadLine());
-            for ( i = amin; i<= amax; i++)
+            char alph = 'A';
+            int ctr = 1;
+            Console.Write("Введите длину пирамиды : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= a; i++)
             {
-                if (i % 9 == 0)
+                for(int j =1; j <= (ctr/2); j++)
                 {
-                    Console.Write("{0} ", i);
-                    sum += i;
+                    Console.Write("{0} ", alph++);
                 }
+                alph--;
+                alph--;
+                for(int j=0; j < (ctr/2); j++)
+                {
+                    Console.Write("{0} ", alph--);
+                }
+                ctr = ctr + 2;
+                alph = 'A';
+                Console.Write("\n");
             }
-            Console.Write("Сумма {0} ", sum);
-            
-
         }    
     }
 }
