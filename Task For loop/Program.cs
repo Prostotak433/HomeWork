ca4f04e25aa570e43d2bc2b251a2c8932adeda60
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-06 17:21:49 +0200

    39. Task ( For Loop )

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -8,16 +8,19 @@ namespace Task_For_loop
         {
             int sum = 0, i;
             Console.Write("Введите число : ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            for ( i = a; i != 0; i = i / 10)
+            int amin = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число : ");
+            int amax = Convert.ToInt32(Console.ReadLine());
+            for ( i = amin; i<= amax; i++)
             {
-                int r = i % 10;
-                sum = sum * 10 + r;
+                if (i % 9 == 0)
+                {
+                    Console.Write("{0} ", i);
+                    sum += i;
+                }
             }
-            if (i == sum)
-                Console.Write("Полиндром {0} ", i);
-            else
-                Console.Write("Не полиндром {0} ", i);
+            Console.Write("Сумма {0} ", sum);
+            
 
         }    
     }
