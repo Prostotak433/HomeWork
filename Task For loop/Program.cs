25c0d19362136d3f4700e46e19eaad4028e0fb76
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-09 17:25:01 +0200

    42. Task ( For Loop )

--- Task For loop/Program.cs
+++ Task For loop/Program.cs
@@ -6,15 +6,19 @@ namespace Task_For_loop
     {
         static void Main(string[] args)
         {
-            int sum = 0, b = 1, c;
-            Console.Write("Введите число");
+            int sum = 0, b = 1,d,p=1, c;
+            Console.Write("Введите число ");
             int a = Convert.ToInt32(Console.ReadLine());
             c = a;
-            for(int i=a; i>0; i=i/2)
+            for(int i=a; i>0; i=i/10)
             {
-                sum = sum + (a % 2) * b;
-                b = b * 10;
-                a = a / 2;
+                d = i % 10;
+                if (b == 1)
+                    p *= 1;
+                else
+                    p *= 2;
+                sum = sum + (d * p);
+                b++;
             }
             Console.Write("число {0} в {1} ", c, sum);
         }    
