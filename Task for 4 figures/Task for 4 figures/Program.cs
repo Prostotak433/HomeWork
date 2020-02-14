03cf0d14af99859a098d9f48cb33cbdcc42da57d
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-14 19:10:25 +0200

    4 figure

---
+++ Task for 4 figures/Task for 4 figures/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_for_4_figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square("ABDC", "Red");
+            square.Display();
+            
+            
+        } 
+    }
+
+    class Figure
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public Figure(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+        public virtual void Display()
+        {
+            Console.Write("Name = {0}, Color = {1} ", Name, Color);
+        }
+    }
+    class Square:Figure
+    {
+        public Square(string name, string color) : base(name, color) { }
+        static int Area()
+        {
+            int a = 3;
+            int s1 = a * a;
+            return s1;
+            
+        }
+        public override void Display()
+        {
+            Console.Write("Name = {0}, Color = {1}, Area = {2} ", Name, Color);
+        }
+    }
+
+}
