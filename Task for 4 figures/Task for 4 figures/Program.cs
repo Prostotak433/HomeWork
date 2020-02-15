4b5b3297ec7d6d49f2796ca4d4938c207456f605
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-15 19:22:55 +0200

    Task. 4 Figure

--- Task for 4 figures/Task for 4 figures/Program.cs
+++ Task for 4 figures/Task for 4 figures/Program.cs
@@ -7,9 +7,17 @@ namespace Task_for_4_figures
         static void Main(string[] args)
         {
             Square square = new Square("Red");
-            double s = square.Area(3);
-            Console.WriteLine(s);
-            
+            double s1 = square.Area(3);
+            Console.Write("Площадь квардата = {0} ",s1);
+            Rectangle rectangle = new Rectangle("Green");
+            double s2 = rectangle.Area(5);
+            Console.Write("\nПлощадь прямоугольника = {0} ", s2);
+            Triangle triangle = new Triangle("Blue");
+            double s3 = triangle.Area(2,3);
+            Console.Write("\nПлощадь треугольника = {0} ", s3);
+            Circle circle = new Circle("Yellow");
+            double s4 = circle.Area(3);
+            Console.Write("\nПлощадь круг = {0} ", s4);
         } 
     }
     class Square
@@ -35,9 +43,9 @@ namespace Task_for_4_figures
     class Triangle:Square
     {
         public Triangle(string color) : base(color) { }
-        public virtual double Area(double a, double h)
+        public virtual double Area(double k, double h)
         {
-            return 1 / 2 * a * h;
+            return (k*h)/2;
         }
     }
     class Circle : Square
