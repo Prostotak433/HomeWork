d0da374b2b66502a08616ec929c654ad1822cfe6
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-02-21 19:06:06 +0200

    Task 4 Figure ( val 2 )

--- Task for 4 figures/Task for 4 figures/Program.cs
+++ Task for 4 figures/Task for 4 figures/Program.cs
@@ -6,82 +6,9 @@ namespace Task_for_4_figures
     {
         static void Main(string[] args)
         {
-            Figure figure = new Figure();
-            Square square = (Square)figure.create("Square");
-            double s1= square.Area();
-            
-            Console.Write("Фигура {0} имеет площадь {1} ", square, s1);
-            
-
+            IFigure square = new Square();
+            IFigure rectangle = new Rectangle();
+            IFigure triangle = new Triangle();
         } 
-    }
-    interface IFigure
-    {
-        double Area();
-    }
-    class Square:IFigure
-    {
-        double a;
-        public Square(double a)
-        {
-            this.a = a;
-        }
-        public  double  Area()
-        {
-            return a * a;
-        }
-    }
-    class Rectangle:IFigure
-    {
-        double c, b;
-        public Rectangle(double c,double b)
-        {
-            this.c = c;
-            this.b = b;
-        }
-        public double Area()
-        {
-            return c * b;
-        }
-    }
-    class Triangle:IFigure
-    {
-        double a, b;
-        public Triangle(double a, double b)
-        {
-            this.a = a;
-            this.b = b;
-        }
-        public double Area()
-        {
-            return (a * b) / 2;
-        }
-    }
-    class Circle:IFigure
-    {
-        double r;
-        public Circle(double r)
-        {
-            this.r = r;
-        }
-        public double Area()
-        {
-            return 3.14 * r * r;
-        }
-    }
-    class Figure
-    {
-        public IFigure create(string typeOfIFigure)
-        {
-            switch(typeOfIFigure)
-            {
-                case "Square": return new Square(3);
-                case "Rectangle:": return new Rectangle(5, 4);
-                case "Triangle": return new Triangle(2,3);
-                case " Circle":return new Circle(3);
-                default:return null;
-
-            }
-        }
     }
 }
