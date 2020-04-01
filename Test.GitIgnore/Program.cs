7b8fdcaa5ffd5f52946b605e61f201d361049d63
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-01 17:15:57 +0300

    53,54. Task.ForLoop

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,8 +7,15 @@ namespace w3resource.Task.ForLoop
         static void Main(string[] args)
         {
             Console.WriteLine("Число в 10 системе ");
-            string Oct = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("{0} : восьмеричная : {1}", Oct, Convert.ToInt64(Oct, 8));
+            string num = Convert.ToString(Console.ReadLine());
+            long temp = Convert.ToInt64(num, 2);
+            Console.WriteLine("{0} : двоичная : {1}", num, Convert.ToString(temp, 8));
+
+            Console.WriteLine("Конвертируем в двоичную систему счисления:\n");
+            string num1 = Convert.ToString(Console.ReadLine());
+            long temp1 = Convert.ToInt64(num1, 8);
+            Console.WriteLine("{0} : восьмеричная : {1}", num1, Convert.ToString(temp1, 2));
+
         }
     }
 }
