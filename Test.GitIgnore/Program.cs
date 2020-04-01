4b97086dc8c66175ebee6327886a0ab092247d14
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-01 16:49:56 +0300

    51. Task.ForLoop

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -6,19 +6,9 @@ namespace w3resource.Task.ForLoop
     {
         static void Main(string[] args)
         {
-            int result = 0, j,i, n1pos, n1;
-            Console.Write("Enter a number to convert : ");
-            n1 = Convert.ToInt32(Console.ReadLine());
-            n1pos = n1;
-            j = 1;
-
-            for (i = n1; i > 0; i = i / 8)
-            {
-                result = result + (i % 8) * j;
-                j = j * 10;
-                n1 = n1 / 8;
-            }
-            Console.Write("\nThe Octal of {0} is {1}.\n\n", n1pos, result);
+            Console.WriteLine("Число в 10 системе ");
+            string Oct = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("{0} : восьмеричная : {1}", Oct, Convert.ToInt64(Oct, 8));
         }
     }
 }
