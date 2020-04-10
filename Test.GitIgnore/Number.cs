74187bc332747c80ed3d338c2141bf03d0c06c96
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-10 19:15:17 +0300

    Task.w3resource.Struct(9)

--- Test.GitIgnore/Number.cs
+++ Test.GitIgnore/Number.cs
@@ -5,16 +5,7 @@ namespace Test.GitIgnore
 {
     struct SructNumber
     {
-        public int n;
-        public int m;
-        public SructNumber(int n1, int m1)
-        {
-            n = n1;
-            m = m1;
-        }
-        public void Info()
-        {
-            Console.WriteLine("m = {0}, n = {1}", m,n);
-        }
+        public string name;
+        public string author;
     }
 }        
--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -6,11 +6,27 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            SructNumber number;
-            number.n = 30;
-            number.m = 40;
-            number.Info();
+            int a = 1;
+            Console.Write("Book: ");
+            int x = int.Parse(Console.ReadLine());
+            SructNumber[] sructNumbers = new SructNumber[x];
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Information of book {0}: ", a);
+
+                Console.Write("Input name of the book : ");
+                sructNumbers[i].name = Console.ReadLine();
+
+                Console.Write("Input the author: ");
+                sructNumbers[i].author = Console.ReadLine();
+                a++;
+                Console.WriteLine();
+            }
+            for(int j =0; j < x; j++)
+            {
+                Console.WriteLine("{0}: Title = {1},  Author = {2}", j + 1, sructNumbers[j].name, sructNumbers[j].author);
+            }
+            Console.Read();
         }
-        
     }
 }
