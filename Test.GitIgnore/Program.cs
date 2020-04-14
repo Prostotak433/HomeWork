2673547c1707c78ca29fb5dcc3b145bb0de4f0a0
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-14 16:05:33 +0300

    Task.w3resource.Basic(40)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -10,12 +10,11 @@ namespace Test.GitIgnore
             int a = int.Parse(Console.ReadLine());
             Console.Write("Введите второе целое число: ");
             int b = int.Parse(Console.ReadLine());
-            Console.Write("Введите третье целое число: ");
-            int c = int.Parse(Console.ReadLine());
+            int number = 20;
+            var res1 = Math.Abs(a - number);
+            var res2 = Math.Abs(b - number);
+            Console.WriteLine(res1 == res2 ? 0 : (res1 > res2 ? b : a));
 
-            Console.WriteLine("Наибольший из трех: " + Math.Max(a, Math.Max(b, c)));
-            Console.WriteLine("Найменьшее из трехч: " + Math.Min(a, Math.Min(b, c)));
-            
         }
     }
 }
