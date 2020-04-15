6aa7fa63894811a711c2ded5a304e264b019feb9
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-15 15:47:37 +0300

    Task.w3resource.ConditionalStatements(1)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Test.GitIgnore
 {
@@ -10,12 +7,13 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("введите строку (содержит не менее одного символа 'w'): ");
-            string a = Console.ReadLine();
-            var count = a.Count(s => s == 'w');
-            Console.WriteLine("Проверка строки, содержащей символ 'w', от 1 до 3 раз:");
-            Console.WriteLine(count >= 1 && count <= 3);
-            
+            Console.Write("Input 1st number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Input 2nd number: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a == b)
+                Console.WriteLine("{0} and {1} are equal", a, b);
+           
         }
     }
 }
