b22720b4f1bc869434b6d2d33a9f529e2fba8dfc
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-15 16:02:14 +0300

    Task.w3resource.ConditionalStatements(4)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -9,13 +9,15 @@ namespace Test.GitIgnore
         {
             Console.Write("Input 1st number: ");
             int a = int.Parse(Console.ReadLine());
-            if (a > 0)
-                Console.WriteLine("{0} is a positive number", a);
+            if (a % 400 == 0)
+                Console.WriteLine("{0} is a leap year.", a);
+            else if (a % 100 == 0)
+                Console.WriteLine("{0} is not a leap year.", a);
+            else if (a % 4 ==0)
+                    Console.WriteLine("{0} is a leap year.", a);
             else
-                Console.WriteLine("{0} is a not potsitive number",a); 
-               
-
-
+                Console.WriteLine("{0} is not a leap year.", a);
+            Console.Read();
         }
     }
 }
