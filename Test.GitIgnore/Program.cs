c0c3a4e4416db1d6b7bcc2ca97dbcc9707a1d75e
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-15 16:06:46 +0300

    Task.w3resource.ConditionalStatements(5)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,17 +7,12 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input 1st number: ");
-            int a = int.Parse(Console.ReadLine());
-            if (a % 400 == 0)
-                Console.WriteLine("{0} is a leap year.", a);
-            else if (a % 100 == 0)
-                Console.WriteLine("{0} is not a leap year.", a);
-            else if (a % 4 ==0)
-                    Console.WriteLine("{0} is a leap year.", a);
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+                Console.WriteLine("Congratulation! You are eligible for casting your vote.");
             else
-                Console.WriteLine("{0} is not a leap year.", a);
-            Console.Read();
+                Console.WriteLine("You are not eligible for casting your vote.");
         }
     }
 }
