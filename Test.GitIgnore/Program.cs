e238108ad12cbbc601a397dab99e0847dc6509ad
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-15 17:53:25 +0300

    Task.w3resource.ConditionalStatements(8)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,17 +7,25 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("The height of a person: ");
-            int m = int.Parse(Console.ReadLine());
+            Console.Write("Input the 1st number : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("nput the 2nd number : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Input the 3rd number : ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
 
-            if (m < 150)
-                Console.WriteLine("The prson is  Dwarf ");
-            else if ((m >= 150) && (m <= 165))
-                Console.WriteLine("The person is  average heighted");
-            else if ((m >= 165.0) && (m <= 195.0))
-                Console.Write("The person is taller. ");
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                    Console.WriteLine("The 1st Number is the greatest among three");
+                else
+                    Console.WriteLine("The 3nd Number is the greatest among three");
+            }
+            else if (num2 > num3)
+                Console.WriteLine("The 2nd Number is the greatest among three");
             else
-                Console.Write("Abnormal height.");
+                Console.WriteLine("The 3nd Number is the greatest among three");
+
         }
     }
 }
