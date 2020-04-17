4dfd2da7dd815d92571ba3d2f665a35bc87fbaf2
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-17 15:11:45 +0300

    Task.w3resource.ConditionalStatements(14)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -13,9 +13,11 @@ namespace Test.GitIgnore
             int a2 = int.Parse(Console.ReadLine());
             Console.Write("3-угол : ");
             int a3 = int.Parse(Console.ReadLine());
-            int res = a1 + a2 + a3;
-            if (res == 180) Console.WriteLine("Можно построить треугольник");
-            else Console.WriteLine("Треугольник недействителен");
+
+            if ((a1 == a2) && (a2 == a3)) Console.WriteLine("This is an equilateral triangle.");
+            else if (a1 == a2 || a1 == a3 || a2 == a3)  Console.Write("This is an isosceles triangle.");
+            else Console.Write("This is a scalene triangle.");
+
         }
     }
 }
