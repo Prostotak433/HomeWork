ca346bf3d511dcca915d37e10b3640f803fcb3f8
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-17 17:20:23 +0300

    Task.w3resource.ConditionalStatements(18)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,22 +7,41 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input Cost Price: ");
-            int start = int.Parse(Console.ReadLine());
-            Console.Write("Input Selling Price: ");
-            int sel = int.Parse(Console.ReadLine());
-            if (sel > start)
+            double coef, sumproc = 0, sumall;
+            Console.Write("Input IDNO: ");
+            double idno = double.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Consumed : ");
+            double cons = double.Parse(Console.ReadLine());
+            if (cons <= 199)
             {
-                int sum = sel - start;
-                Console.WriteLine("You can booked your profit amount: {0}", sum);
+                coef = 1.20;
             }
-            else if (start > sel)
+            else if (cons >= 200 && cons < 400)
             {
-                int sum = start - sel;
-                Console.Write("You got a loss of amount : {0}", sum);
+                 coef = 1.50;
             }
-            else Console.WriteLine("You are running in no profit no loss condition.");
+            else if (cons >= 400 && cons < 600)
+            {
+                 coef = 1.80;
+            }
+            else
+            {
+                coef = 2.00;
+            }
+            double sum = cons * coef;
+            if (sum > 300)
+                sumproc = (sum * 15) / 100;
+            sumall = sumproc + sum;
+            Console.Write("\nCustomer IDNO                    :{0}\n",idno);
+            Console.Write("Customer Name                      :{0}\n", name);
+            Console.Write("unit Consumed                      :{0}\n", cons);
+            Console.Write("Amount Charges @Rs. {0} per unit   :{1}\n", coef, sum);
+            Console.Write("Surchage Amount                    :{0}\n", sumproc);
+            Console.Write("Net Amount Paid By the Customer    :{0}\n", sumall);
         }
+        
     }
 }
 
