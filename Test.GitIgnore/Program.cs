99ee9a58d920f538479ad79003a83165db1e54b4
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-17 15:42:54 +0300

    Task.w3resource.ConditionalStatements(16)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,17 +7,35 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("1-угол : ");
-            int a1 = int.Parse(Console.ReadLine());
-            Console.Write("2-угол : ");
-            int a2 = int.Parse(Console.ReadLine());
-            Console.Write("3-угол : ");
-            int a3 = int.Parse(Console.ReadLine());
-
-            if ((a1 == a2) && (a2 == a3)) Console.WriteLine("This is an equilateral triangle.");
-            else if (a1 == a2 || a1 == a3 || a2 == a3)  Console.Write("This is an isosceles triangle.");
-            else Console.Write("This is a scalene triangle.");
-
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            char ch = char.Parse(Console.ReadLine().ToLower());
+            int i = ch;
+            if (i >= 47 && 0 <= 66) Console.Write("You entered a number, Please enter an alpahbet.");
+            else
+            {
+                switch(ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is not a vowel");
+                        break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
