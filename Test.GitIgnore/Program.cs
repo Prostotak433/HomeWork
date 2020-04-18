281f1e16b090aaae25e8daebdfd0fc02c77d5809
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-18 15:20:29 +0300

    Task.w3resource.ConditionalStatements(23)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -9,29 +9,46 @@ namespace Test.GitIgnore
         {
             Console.Write("Input Month(1-12) : ");
             int num = int.Parse(Console.ReadLine());
-
             switch (num)
             {
                 case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    Console.WriteLine("Month have 31 days");
+                    Console.Write("January\n");
                     break;
                 case 2:
-                    Console.WriteLine("Month have 28-29 days");
+                    Console.Write("February\n");
+                    break;
+                case 3:
+                    Console.Write("March\n");
                     break;
                 case 4:
+                    Console.Write("April\n");
+                    break;
+                case 5:
+                    Console.Write("May\n");
+                    break;
                 case 6:
+                    Console.Write("June\n");
+                    break;
+                case 7:
+                    Console.Write("July\n");
+                    break;
+                case 8:
+                    Console.Write("August\n");
+                    break;
                 case 9:
+                    Console.Write("September\n");
+                    break;
+                case 10:
+                    Console.Write("October\n");
+                    break;
                 case 11:
-                    Console.WriteLine("Month have 30 days");
+                    Console.Write("November\n");
+                    break;
+                case 12:
+                    Console.Write("December\n");
                     break;
                 default:
-                    Console.Write("invalid Month number.\nPlease try again ....\n");
+                    Console.Write("invalid Month number. \nPlease try again ....\n");
                     break;
             }
         }
