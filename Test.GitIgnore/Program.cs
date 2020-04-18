5ad984e2d2f8244331d99be09322cd5dd122c8d8
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-18 14:55:00 +0300

    Task.w3resource.ConditionalStatements(19)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -6,40 +6,32 @@ namespace Test.GitIgnore
     class Program
     {
         static void Main(string[] args)
-        {
-            double coef, sumproc = 0, sumall;
-            Console.Write("Input IDNO: ");
-            double idno = double.Parse(Console.ReadLine());
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Consumed : ");
-            double cons = double.Parse(Console.ReadLine());
-            if (cons <= 199)
+        { 
+            Console.Write("Input the grade : ");
+            char grade = char.Parse(Console.ReadLine().ToUpper());
+            string a1;
+            switch(grade)
             {
-                coef = 1.20;
+                case 'E':
+                    a1 =("Excellent");
+                    break;
+                case 'V':
+                    a1 =("Very good");
+                    break;
+                case 'G':
+                    a1 = ("Good");
+                    break;
+                case 'A':
+                    a1 = ("Average");
+                    break;
+                case 'F':
+                    a1 =("Fail");
+                    break;
+                default:
+                    a1 = "Invalid Grade Found.";
+                    break;
             }
-            else if (cons >= 200 && cons < 400)
-            {
-                 coef = 1.50;
-            }
-            else if (cons >= 400 && cons < 600)
-            {
-                 coef = 1.80;
-            }
-            else
-            {
-                coef = 2.00;
-            }
-            double sum = cons * coef;
-            if (sum > 300)
-                sumproc = (sum * 15) / 100;
-            sumall = sumproc + sum;
-            Console.Write("\nCustomer IDNO                    :{0}\n",idno);
-            Console.Write("Customer Name                      :{0}\n", name);
-            Console.Write("unit Consumed                      :{0}\n", cons);
-            Console.Write("Amount Charges @Rs. {0} per unit   :{1}\n", coef, sum);
-            Console.Write("Surchage Amount                    :{0}\n", sumproc);
-            Console.Write("Net Amount Paid By the Customer    :{0}\n", sumall);
+            Console.Write("You have chosen  : {0}\n", a1);
         }
         
     }
