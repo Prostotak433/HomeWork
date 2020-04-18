25d4a3854bed11560d75cfd33349a8d9c27f70fe
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-18 15:07:21 +0300

    Task.w3resource.ConditionalStatements(21)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,28 +7,46 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input Day :");
-            int day = int.Parse(Console.ReadLine());
-            switch (day)
+            Console.Write("Input Digit(0-9) : ");
+            int num = int.Parse(Console.ReadLine());
+
+            switch (num)
             {
-                case 1: Console.WriteLine("Ponedel");
+                case 0:
+                    Console.Write("Zero\n");
                     break;
-                case 2: Console.WriteLine("Vtornik");
+
+                case 1:
+                    Console.Write("one\n");
                     break;
-                case 3: Console.WriteLine("Sreda");
+                case 2:
+                    Console.Write("Two\n");
                     break;
-                case 4: Console.WriteLine("Chetverg");
+                case 3:
+                    Console.Write("Three\n");
                     break;
-                case 5: Console.WriteLine("Pyatnica");
+                case 4:
+                    Console.Write("Four\n");
                     break;
-                case 6: Console.WriteLine("Sybota");
+                case 5:
+                    Console.Write("Five\n");
                     break;
-                case 7: Console.WriteLine("Voskrec");
+                case 6:
+                    Console.Write("Six\n");
                     break;
-                default: Console.WriteLine("Hz 4to tu hochew");
+                case 7:
+                    Console.Write("Seven\n");
+                    break;
+                case 8:
+                    Console.Write("Eight\n");
+                    break;
+                case 9:
+                    Console.Write("Nine\n");
+                    break;
+                default:
+                    Console.Write("invalid digit. \nPlease try again ....\n");
                     break;
             }
-        }
     }
 }
 
