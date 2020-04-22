092e188a1d3514da3dde1613bfc411611f7860c8
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-22 18:00:32 +0300

    Task.w3resource.DataTypes(11)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,12 +7,9 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input first number: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.Write("Input second number: ");
-            int num2 = int.Parse(Console.ReadLine());
-            bool res = ((num1 % 2 == 0) && (num2 % 2 == 0)) ? true : false;
-            Console.WriteLine(res);
+            Console.Write("Input number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(a, 2));
         }
     }
 }
