254aa1054be3d37c91a0bf9380d3b11a0efa715f
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-22 17:49:01 +0300

    Task.w3resource.DataTypes(8)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,23 +7,11 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input distance(meters): ");
-            float distance = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Input timeSec(hour): ");
-            float hour = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Input timeSec(minutes): ");
-            float min = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Input timeSec(seconds): ");
-            float sec = Convert.ToSingle(Console.ReadLine());
-            float timeSec = (hour * 3600) + (min * 60) + sec;
-            float mps = distance / timeSec;
-            float kph = (distance / 1000.0f) / (timeSec / 3600.0f);
-            float mph = kph / 1.609f;
-
-            Console.WriteLine("Your speed in meters/sec is {0}", mps);
-            Console.WriteLine("Your speed in km/h is {0}", kph);
-            Console.WriteLine("Your speed in miles/h is {0}", mph);
-
+            Console.Write("Radius: ");
+            double r = double.Parse(Console.ReadLine());
+            const double PI = 3.14;
+            Console.WriteLine(4 * PI * (r * r));
+            Console.WriteLine(4/3 * PI * (r * r * r));
         }
     }
 }
