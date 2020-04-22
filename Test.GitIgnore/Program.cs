68fd03834f8e8b553269e324089ff84235edb572
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-22 17:54:10 +0300

    Task.w3resource.DataTypes(10)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,14 +7,12 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input a symbol: ");
-            char symvol = Convert.ToChar(Console.ReadLine());
-            if ((symvol == 'a') || (symvol == 'e') || (symvol == 'i') || (symvol == 'o') || (symvol == 'u'))
-                Console.WriteLine("It's a lowercase vowel.");
-            else if ((symvol >= '0') && (symvol <= '9'))
-                Console.WriteLine("It's a digit.");
-            else
-                Console.Write("It's another symbol.");
+            Console.Write("Input first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Input second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            bool res = ((num1 % 2 == 0) && (num2 % 2 == 0)) ? true : false;
+            Console.WriteLine(res);
         }
     }
 }
