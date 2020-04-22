ab2e4028e529d932c1126570b9254c974a5fde08
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-22 17:43:00 +0300

    Task.w3resource.DataTypes(7)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,13 +7,23 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("x = y² - 2y +1");
-            int x = 0;
-            for(int y = -5; y <= 5; y++)
-            {
-                x = y * y - 2 * y + 1;
-                Console.WriteLine("y = {0}; x = {0}²-2*{0}+1 = {1}", y, x);
-            }
+            Console.Write("Input distance(meters): ");
+            float distance = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Input timeSec(hour): ");
+            float hour = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Input timeSec(minutes): ");
+            float min = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Input timeSec(seconds): ");
+            float sec = Convert.ToSingle(Console.ReadLine());
+            float timeSec = (hour * 3600) + (min * 60) + sec;
+            float mps = distance / timeSec;
+            float kph = (distance / 1000.0f) / (timeSec / 3600.0f);
+            float mph = kph / 1.609f;
+
+            Console.WriteLine("Your speed in meters/sec is {0}", mps);
+            Console.WriteLine("Your speed in km/h is {0}", kph);
+            Console.WriteLine("Your speed in miles/h is {0}", mph);
+
         }
     }
 }
