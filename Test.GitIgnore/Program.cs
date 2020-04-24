e6b2f1b0253aede5ab822620f850238211b42427
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-24 17:52:06 +0300

    Task.w3resource.ForLoop(57)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,31 +7,15 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            int flg3=0;
-            Console.Write("Input  a positive integer: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 3; i <= num / 2; i++)
+           string str1 = "";
+            Console.Write("Input  A String : ");
+            string str = Console.ReadLine();
+            int l = str.Length - 1;
+            for (int i = l; i >= 0; i--)
             {
-                int flg1 = 1;
-                int flg2 = 1;
-                for (int j = 2; j < i; j++)
-                {
-                    if (i % j == 0)
-                    { flg1 = 0; j = i; }
-                }
-                for (int j = 2; j < num - i; j++)
-                {
-                    if ((num - i) % j == 0)
-                    { flg2 = 0; j = num - i; }
-                }
-                if (flg1 == 1 && flg2 == 1)
-                {
-                    Console.Write("{0} =  {1} + {2}  \n", num, i, num - i);
-                     flg3 = 1;
-                }
+                 str1 = str1 + str[i];
             }
-            if (flg3 == 0)
-            { Console.Write("\n{0} can not be expressed as sum of two prime numbers.\n\n", num); }
+            Console.WriteLine(str1);
         }
     }
 }
