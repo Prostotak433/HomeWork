468ed4a1875687bd9e56737528591f647fd8adf5
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-24 16:50:25 +0300

    Task.w3resource.ForLoop(55)

--- Test.GitIgnore/Program.cs
+++ Test.GitIgnore/Program.cs
@@ -7,9 +7,25 @@ namespace Test.GitIgnore
     {
         static void Main(string[] args)
         {
-            Console.Write("Input number: ");
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(a, 2));
+            Console.Write("Input  any Decimal number: ");
+            int decn = Convert.ToInt32(Console.ReadLine());
+            int q = decn;
+            for (int i = q; i > 0; i = i / 16)
+            {
+                int tmp = i % 16;
+                if (tmp < 10)
+                    tmp = tmp + 48;
+                else
+                    tmp = tmp + 55;
+                int dn = dn * 100 + tmp;
+            }
+            Console.Write("\nThe equivalent Hexadecimal Number : ");
+            for (int j = dn; j > 0; j = j / 100)
+            {
+                int s = j % 100;
+                Console.Write("{0}", (char)s);
+            }
+            Console.Write("\n\n");
         }
     }
 }
