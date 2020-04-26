97c6365137f9eb32460196414396c4e6d5f7d1df
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-26 18:12:00 +0300

    Task.w3resource.ForLoop(65)

--- Task.w3resource/Program.cs
+++ Task.w3resource/Program.cs
@@ -10,7 +10,7 @@ namespace Task.w3resource
             {
                 for (int u = 0; u <= 6; u++)
                 {
-                    if ((u == 1 && i != 0 && i != 6) || ((i == 0 || i == 6) && u > 1 && u < 5) || (i == 3 && u > 2 && u < 6)||(u == 5 && i != 0 && i!= 2 && i != 6))
+                    if ((u == 1 || u == 5) || (i == 3 && (u > 1 && u < 5 )))
                         Console.Write("*");
                     else
                         Console.Write(" ");
