05e55347d560fb84be1f580700f34a13ac143241
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-27 16:20:14 +0300

    Task.w3resource.ForLoop(69)

--- Task.w3resource/Program.cs
+++ Task.w3resource/Program.cs
@@ -11,15 +11,8 @@ namespace Task.w3resource
             {
                 for (int u = 0; u <= 6; u++)
                 {
-                    if (u == 1 || ((i == u + 1) && u != 0))
+                    if (u == 1 || (i == 6 && u > 1 && u < 6))
                         Console.Write("*");
-
-                    else if (i == ip && u == k)
-                    {
-                        Console.Write("*");
-                        ip = ip + 1;
-                        k = k - 1;
-                    }
                     else
                         Console.Write(" ");
                 }
