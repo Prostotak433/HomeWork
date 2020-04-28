5c863accfd8c26f4e5a1224b72d8e7128eeaa376
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-28 16:12:42 +0300

    Task.w3resource.Array(2)

--- Task.w3resource/Program.cs
+++ Task.w3resource/Program.cs
@@ -6,19 +6,25 @@ namespace Task.w3resource
     {
         static void Main(string[] args)
         {
-            int[] mass = new int[10];
-            Console.WriteLine("Input 10 elements in the array: ");
-            for (int i = 0; i < 10; i++)
+            int[] mass = new int[100];
+            Console.Write("Input the number of elements to store in the array : ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input {0} elements in the array: ", num);
+            for (int i = 0; i < num; i++)
             {
                 Console.Write("element - {0} : ", i);
                 mass[i] = int.Parse(Console.ReadLine());
             }
             Console.WriteLine("Elements in array are: ");
-            for(int i = 0; i < 10; i++)
+            for(int i = 0; i < num; i++)
             {
                 Console.Write("{0} ", mass[i]);
             }
-
+            Console.WriteLine("\nThe values store into the array in reverse are: ");
+            for(int i = num - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", mass[i]);
+            }
         }   
     }
 }
