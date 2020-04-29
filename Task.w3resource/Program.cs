21950e36f4ac8b467a4b40fd5b6fce3070751a5a
Prostotak433 <53277177+Prostotak433@users.noreply.github.com>
2020-04-29 15:53:02 +0300

    Task.w3resource.Array(5)

--- Task.w3resource/Program.cs
+++ Task.w3resource/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Task.w3resource
 {
@@ -6,31 +8,30 @@ namespace Task.w3resource
     {
         static void Main(string[] args)
         {
+            int count = 0;
             int[] mass1 = new int[100];
-            int[] mass2 = new int[100];
             Console.Write("Input the number of elements to be stored in the array : ");
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine("Input {0} elements in the array: ", num);
-            for(int i = 0; i < num; i++)
-            {
-                Console.Write("element - {0} : ",i);
-                mass1[i] = int.Parse(Console.ReadLine());
-            }
-            for(int i = 0; i < num; i++)
-            {
-                mass2[i] = mass1[i];
-            }
-            Console.Write("The elements stored in the first array are : ");
-            for(int i = 0; i < num; i++)
-            {
-                Console.Write("{0}  ", mass1[i]);
-            }
-            Console.Write("\n\nThe elements copied into the second array are :\n");
             for (int i = 0; i < num; i++)
             {
-                Console.Write("{0}  ", mass2[i]);
+                Console.Write("element - {0} : ", i);
+                mass1[i] = int.Parse(Console.ReadLine());
             }
-        }   
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = i+1 ; j < num; j++)
+                {
+                    if (mass1[i] == mass1[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("The number of Duplicate Elements Present in Array is = {0} ", count);
+            Console.ReadLine();
+        }
     }
 }
         
